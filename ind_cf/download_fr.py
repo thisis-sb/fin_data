95c3754a11de0f02b4b179d17ea0415ff9944853
Sunil Bhat <iamsunilbhat@gmail.com>
2023-03-02 09:57:05 +0530

    CHG_ID_00041

--- ind_cf/download_fr.py
+++ ind_cf/download_fr.py
@@ -1,154 +1,168 @@
 """
-Download CF FRs & create XBRL archive & metadata to access that
-Usage: n_to_download exchange
-Open Issues / TO DO:
-    1. What to do with errors later on (if they're fixed)
+Download CF FRs & create JSON/XBRL archives & metadata to access that
 """
 ''' --------------------------------------------------------------------------------------- '''
 
-import sys
 import os
-import glob
-import datetime
+from datetime import datetime
+import traceback
 import pandas as pd
-import pygeneric.archiver as pyg_archiver
 import pygeneric.misc as pyg_misc
-import base_utils
+import pygeneric.http_utils as pyg_http_utils
+import pygeneric.archiver as pyg_archiver
 
-PATH_1 = os.path.join(os.getenv('DATA_ROOT'), '01_fin_data/02_ind_cf')
+PATH_1 = os.path.join(os.getenv('DATA_ROOT'), '02_ind_cf/01_nse_fr_filings')
+PATH_2 = os.path.join(os.getenv('DATA_ROOT'), '02_ind_cf/02_nse_fr_archive')
 
 ''' --------------------------------------------------------------------------------------- '''
-def current_asn(xp):
-    al = glob.glob('%s/**/**/archive_*' % xp)
-    assert len(al) > 0, f'{xp}: Something went really bad. len(al): {len(al)}'
-    current_archive_path = sorted(al)[-1]
-    return int(os.path.basename(current_archive_path).split('_')[1])
+def prepare_key(row_dict):
+    params = row_dict['params']
+    if '&' in row_dict['symbol']:
+        params = params.replace('&', '%26')
+    seqNumber = row_dict['seqNumber']
+    industry = row_dict['industry'] if pd.notna(row_dict['industry']) else ''
+    # oldNewFlag = row_dict['oldNewFlag'] if row_dict['oldNewFlag'] is not None else ''
+    oldNewFlag = row_dict['oldNewFlag'] if pd.notna(row_dict['oldNewFlag']) else ''
+    reInd = row_dict['reInd']
+    format_x = row_dict['format']
 
-def get_asf(asn, divider):
-    """ Logic: divmod(asn, divider=ARCHIVES_PER_SUB_FOLDER) """
-    return ('%d' % divmod(asn, divider)[0]).zfill(2)
+    key = 'params=%s&seq_id=%s' % (params, seqNumber) + \
+          '&industry=%s&frOldNewFlag=%s' % (industry, oldNewFlag) + \
+          '&ind=%s&format=%s' % (reInd, format_x)
+
+    return key
+
+''' --------------------------------------------------------------------------------------- '''
+class Manager:
+    def __init__(self, year):
+        self.year = year
+        self.json_checkpoint_step = 50
+        self.json_incremental_downloads = 0
+        self.session_errors = 0
+        self.json_archives = {}
+        self.json_metadata_dict = {}
+        self.json_base_url = 'https://www.nseindia.com/api/' +\
+                             'corporates-financial-results-data?index=equities&'
+        self.run_timestamp = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+        self.json_metadata_filename = os.path.join(PATH_2, '%d' % year, 'metadata_json.csv')
+
+        fr_filings = pd.read_csv(os.path.join(PATH_1, 'CF_FR_%d.csv' % yr))
+        fr_filings['filingDate'] = pd.to_datetime(fr_filings['filingDate'])
+        print('\nLoaded fr_filings. Shape:', fr_filings.shape)
+
+        if os.path.exists(self.json_metadata_filename):
+            df = pd.read_csv(self.json_metadata_filename)
+            df['filingDate'] = pd.to_datetime(df['filingDate'])
+            self.json_metadata_dict = dict(zip(df['key'], df.to_dict('records')))
+            fr_filings['key'] = fr_filings.apply(lambda x: prepare_key(x), axis=1)
+            self.to_download = fr_filings.loc[~fr_filings['key'].isin(df['key'].unique())].\
+                reset_index(drop=True)
+        else:
+            # metadata_df = pd.DataFrame()
+            results_dict = {}
+            self.to_download = fr_filings.copy()
+
+        self.http_obj = pyg_http_utils.HttpDownloads(max_tries=10, timeout=10) \
+            if self.to_download.shape[0] > 0 else None
+
+    def download_all(self, max_downloads=5000):
+        if self.to_download.shape[0] == 0:
+            print('Nothing to download')
+            return
+
+        print('DownloadManager initialized. To download: %d' % self.to_download.shape[0])
+        n_downloaded = 0
+        for idx in self.to_download.index:
+            pyg_misc.print_progress_str(idx + 1, self.to_download.shape[0])
+            if idx % 50 == 0:
+                print(' --> n_downloaded/session_errors: %d/%d'
+                      % (n_downloaded, self.session_errors))
+            self.download(idx)
+            n_downloaded += 1
+            if n_downloaded >= max_downloads:
+                break
+        self.flush_json()
+
+        print('\nDownloads finished --> n_downloaded/session_errors: %d/%d'
+              % (n_downloaded, self.session_errors))
+        saved_metadata = pd.read_csv(self.json_metadata_filename)
+        print('saved_metadata: %d rows, %d errors'
+              % (saved_metadata.shape[0], saved_metadata.loc[~saved_metadata['outcome']].shape[0]))
+        return
+
+    def download(self, fr_idx):
+        row = self.to_download.loc[fr_idx].to_dict()
+        key = prepare_key(row)
+        if key not in list(self.json_metadata_dict.keys()):
+            if self.download_json(row, key):
+                self.json_incremental_downloads += 1
+                if self.json_incremental_downloads >= self.json_checkpoint_step:
+                    self.flush_json()
+        return True
+
+    def download_json(self, row, key):
+        filingDate = row['filingDate']
+        filingYear = int(filingDate.year)
+        filingMonth = int(filingDate.month)
+        assert self.year == filingYear, 'Years not matching'
+
+        json_url = self.json_base_url + key
+        try:
+            json_data, raw_data = self.http_obj.http_get_both(json_url)
+            assert ((json_data['seqnum']) == str(row['seqNumber']) and
+                    (json_data['longname'] == row['companyName'])), \
+                '[%s][%s][%s][%s]\n%s' % (json_data['seqnum'], row['seqNumber'],
+                                          json_data['longname'], row['companyName'], json_url)
+            outcome, resultFormat, error_msg, trace_log = True, json_data['resultFormat'], '', ''
+        except Exception as e:
+            outcome, error_msg, trace_log = False, 'ERROR! %s' % e, traceback.format_exc()
+            json_data, resultFormat, raw_data = '', '', ''
+            self.session_errors += 1
+
+        archive_sub_path = '%d/json_month_%s' % (self.year, ('%d' % filingMonth).zfill(2))
+        if archive_sub_path not in self.json_archives.keys():
+            archive_path = os.path.join(PATH_2, archive_sub_path)
+            update = os.path.exists(archive_path)
+            self.json_archives[archive_sub_path] = pyg_archiver.Archiver(archive_path, mode='w',
+                                                                         update=update)
+        # should we check if the key exists - for earlier erroneous cases?
+        self.json_archives[archive_sub_path].add(key, raw_data)
+
+        ''' TO DO: add more relevant columns to this - for easier use later '''
+        self.json_metadata_dict[key] = {'symbol': row['symbol'],
+                                        'isin': row['isin'],
+                                        'consolidated': row['consolidated'],
+                                        'period': row['period'],
+                                        'toDate': row['toDate'],
+                                        'relatingTo': row['relatingTo'],
+                                        'seqNumber': row['seqNumber'],
+                                        'resultFormat': resultFormat,
+                                        'filingDate': filingDate,
+                                        'filingYear': filingYear,
+                                        'filingMonth': filingMonth,
+                                        'key': key,
+                                        'outcome': outcome,
+                                        'size': len(raw_data),
+                                        'archive_sub_path': archive_sub_path,
+                                        'error_msg': error_msg,
+                                        'trace_log': trace_log,
+                                        'json_url': json_url,
+                                        'run_timestamp': self.run_timestamp
+                                        }
+        return True
+
+    def flush_json(self):
+        for k in self.json_archives.keys():
+            self.json_archives[k].flush(create_parent_dir=True)
+        self.json_archives.clear()
+        self.json_incremental_downloads = 0
+        df = pd.DataFrame(list(self.json_metadata_dict.values()))
+        df.sort_values(by='filingDate', inplace=True)
+        df.to_csv(self.json_metadata_filename, index=False)
+        return True
 
 ''' --------------------------------------------------------------------------------------- '''
 if __name__ == '__main__':
-    n_to_download = 5000 if len(sys.argv) != 2 else int(sys.argv[1])
-    exchange = 'nse' if len(sys.argv) != 3 else sys.argv[2]
-
-    fr_filings_df = base_utils.load_filings_fr(
-        os.path.join(PATH_1, f'{exchange}_fr_filings/CF_FR_*.csv'))
-    fr_filings_df['filingDate'] = pd.to_datetime(fr_filings_df['filingDate'])
-    fr_filings_df.sort_values(by='filingDate')
-    fr_filings_df.reset_index(drop=True, inplace=True)
-
-    ARCHIVE_FOLDER = os.path.join(PATH_1, f'{exchange}_fr_xbrl_archive')
-    METADATA_FILENAME = os.path.join(ARCHIVE_FOLDER, 'metadata_S1.csv')
-
-    ''' Important:
-    50 archives per sub_folder. 50 xbrl files per archive.
-    With 20 sub_folders, can store 100000 xbrl files --> Ok for 3 more years (across 40 sub_folders)
-    '''
-    ARCHIVES_PER_SUB_FOLDER = 50
-    ARCHIVE_MAX_SIZE = 50
-
-    if os.path.exists(METADATA_FILENAME):
-        metadata_df = pd.read_csv(METADATA_FILENAME)
-        df = fr_filings_df.loc[~fr_filings_df['xbrl'].isin(metadata_df['xbrl'].unique())]
-        xbrl_links_to_download = df['xbrl'].unique()
-        archive_sequence_number = current_asn(ARCHIVE_FOLDER)
-    else:
-        metadata_df = pd.DataFrame()
-        xbrl_links_to_download = fr_filings_df['xbrl'].unique()
-        archive_sequence_number = 0
-    print('archive_sequence_number:', archive_sequence_number)
-    print('metadata_df.shape:', metadata_df.shape)
-    print('In all, %d UN-downloaded xbrl links' % len(xbrl_links_to_download))
-
-    print('\nXBRL downloads START ...')
-    run_timestamp = datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
-    xbrl_archive, archive_path, n_downloaded, n_errors = None, None, 0, 0
-    for idx, xbrl_url in enumerate(xbrl_links_to_download):
-        pyg_misc.print_progress_str(idx + 1, len(xbrl_links_to_download))
-
-        if os.path.basename(xbrl_url) == '-' or xbrl_url not in xbrl_links_to_download:
-            assert False, f'Something went wrong! {xbrl_url}'
-
-        filing_date = fr_filings_df.loc[fr_filings_df['xbrl'] == xbrl_url, 'filingDate'].values[-1]
-        fileName    = fr_filings_df.loc[fr_filings_df['xbrl'] == xbrl_url, 'fileName'].values[-1]
-        filingYear  = 'year_%s' % fileName.split('_')[-1].split('.')[0]
-
-        try:
-            xbrl_data = base_utils.get_xbrl(xbrl_url)
-            outcome, error_msg = True, ''
-        except Exception as e:
-            outcome, error_msg = False, 'ERROR! %s' % e
-            xbrl_data = ''
-            n_errors += 1
-
-        if xbrl_archive is None:
-            """
-            1. check if archive with current sequence number exists.
-            2. if yes, open it and check if it has space. If yes, continue to use it.
-            3. if not, increase sequence number and create a new archive
-            """
-            archive_name = 'archive_%s' % ('%d' % archive_sequence_number).zfill(3)
-            archive_sub_folder = get_asf(archive_sequence_number, divider=ARCHIVES_PER_SUB_FOLDER)
-
-            archive_path = os.path.join(ARCHIVE_FOLDER, '%s/%s/%s'
-                                        % (filingYear, archive_sub_folder, archive_name))
-
-            if os.path.exists(archive_path) and pyg_archiver.\
-                    Archiver(archive_path, mode='r').size() < ARCHIVE_MAX_SIZE:
-                update = True
-            else:
-                if os.path.exists(archive_path) and pyg_archiver.\
-                        Archiver(archive_path, mode='r').size() == ARCHIVE_MAX_SIZE:
-                    archive_sequence_number += 1  # messy for now
-                archive_name = 'archive_%s' % ('%d' % archive_sequence_number).zfill(3)
-                archive_sub_folder = get_asf(archive_sequence_number, divider=ARCHIVES_PER_SUB_FOLDER)
-                archive_path = os.path.join(ARCHIVE_FOLDER, '%s/%s/%s'
-                                            % (filingYear, archive_sub_folder, archive_name))
-                update = False
-            xbrl_archive = pyg_archiver.Archiver(archive_path, mode='w', update=update)
-
-        xbrl_archive.add(xbrl_url, xbrl_data)
-        metadata_df = pd.concat(
-            [metadata_df, pd.DataFrame({'xbrl':xbrl_url,
-                                        'filingYear':filingYear,
-                                        'filingDate':filing_date,
-                                        'archive_path': archive_path,
-                                        'size': len(xbrl_data),
-                                        'outcome':outcome,
-                                        'error_msg':error_msg,
-                                        'run_timestamp':run_timestamp
-                                        }, index=[0])])
-
-        if xbrl_archive.size() >= ARCHIVE_MAX_SIZE:
-            ''' perform checkpoint '''
-            filingYearDir = os.path.dirname(os.path.dirname(archive_path))
-            if not os.path.exists(filingYearDir):
-                os.mkdir(filingYearDir)
-            xbrl_archive.flush(create_parent_dir=True)
-            metadata_df['filingDate'] = pd.to_datetime(metadata_df['filingDate'])
-            metadata_df.sort_values(by='filingDate', inplace=True)
-            metadata_df.to_csv(METADATA_FILENAME, index=False)
-            xbrl_archive, archive_path = None, None  # make sure
-            archive_sequence_number += 1  # see messy above
-
-        n_downloaded += 1
-        if n_downloaded >= n_to_download:
-            break
-
-    if xbrl_archive is not None and n_downloaded > 0:
-        ''' last flush '''
-        filingYearDir = os.path.dirname(os.path.dirname(archive_path))
-        if not os.path.exists(filingYearDir):
-            os.mkdir(filingYearDir)
-        xbrl_archive.flush(create_parent_dir=True)
-        metadata_df['filingDate'] = pd.to_datetime(metadata_df['filingDate'])
-        metadata_df.sort_values(by='filingDate', inplace=True)
-        metadata_df.to_csv(METADATA_FILENAME, index=False)
-        xbrl_archive, archive_path = None, None  # make sure
-
-    print('\nXBRL downloads END')
-    print('\n%d xbrl\'s downloaded in this run. %d errors' % (n_downloaded, n_errors))
-    print('Whole metadata_df: %d rows, %d errors'
-          % (metadata_df.shape[0], metadata_df.loc[~metadata_df['outcome']].shape[0]))
+    yr = 2023
+    mgr = Manager(yr)
+    mgr.download_all()
