b8b370cedcbec3ae0e8b873618bb6c5a6f24ad7f
thisis-sb <iamsunilbhat@gmail.com>
2024-08-27 11:27:34 +0530

    change 000124

--- others/amfi_navs.py
+++ others/amfi_navs.py
@@ -7,20 +7,17 @@ from fin_data.env import *
 import os
 import sys
 import pandas as pd
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 ''' --------------------------------------------------------------------------------------- '''
-def get_raw_amfi_data(as_on_date):
-    date_from = as_on_date - timedelta(days=7)
+def get_all_navs(as_on_date:str):
+    date_from = datetime.strptime(as_on_date, '%d-%b-%Y') - timedelta(days=7)
     url_base = 'https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx'
-    url = f'{url_base}?frmdt=%s&todt=%s' % (date_from.strftime('%d-%b-%Y'),
-                                            as_on_date.strftime('%d-%b-%Y'))
+    url = f'{url_base}?frmdt=%s&todt=%s' % (date_from.strftime('%d-%b-%Y'), as_on_date)
 
     df = pd.read_csv(url, sep=';')
-    assert df.shape[0] > 0, print('ERROR! get_raw_amfi_data: No raw data retrieved')
-
-    raw_navs = df[df['Date'].notna()].reset_index(drop=True)
-    assert raw_navs.shape[0] > 0, print('ERROR! get_raw_amfi_data: No valid data found')
+    assert df.shape[0] > 0, 'ERROR! get_all_navs: No raw data retrieved'
+    assert df[df['Date'].notna()].shape[0] > 0, 'ERROR! get_all_navs: No valid data found'
 
     df.rename(columns={'Scheme Code': 'CODE',
                        'ISIN Div Payout/ISIN Growth': 'ISIN',
@@ -28,7 +25,7 @@ def get_raw_amfi_data(as_on_date):
                        }, inplace=True)
     df = df[['CODE', 'ISIN', 'NAV', 'Date', 'Scheme Name']]
 
-    df = df.groupby(['CODE', 'ISIN']).tail(1)
+    df = df.groupby(['CODE', 'ISIN']).tail(1).reset_index(drop=True)
 
     return df
 
@@ -56,7 +53,7 @@ if __name__ == '__main__':
         if os.path.isfile(f):
             print('already exists.')
         else:
-            raw_navs = get_raw_amfi_data(d)
+            raw_navs = get_all_navs(d.strftime('%d-%b-%Y'))
             os.makedirs(os.path.dirname(f), exist_ok=True)
             raw_navs.to_csv(f, index=False)
             print('downloaded & saved. shape:', raw_navs.shape)
